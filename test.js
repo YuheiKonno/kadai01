3bd4383944305163e40f45d68e9f3d86c7caad3f
YuheiKonno <tech0718@gmail.com>
2019-02-10 18:54:23 +0900

    First comitt

---
+++ test.js
@@ -0,0 +1,43 @@
+// // alert(11111);
+
+// console.log("初めてののジャバスクリプト");
+// console.log(23+5);
+// console.log(2000-1800);
+// console.log("18+5");
+
+// var name = '今野';
+// console.log('自分の名前を表示');
+// console.log("name");
+
+
+// var point=Math.floor(Math.random() *100);
+// if (point >= 80) {
+//     console.log('素晴らしい');
+//   } else {
+//     console.log('もっと頑張りましょう');
+//   }
+
+
+//   var m=Math.floor(Math.random() *5+1);
+//   console.log(m);
+
+//   var points=Math.floor(Math.random() *5+1);
+//   console.log(points);
+// if (points = 1) {
+//     console.log('1だよ');
+//   } else if (points =2){
+//     console.log('2だよ');
+//   } else if (points =3){
+//       console.log('３だよ');
+// 　} else if (points =4){
+//         console.log('4だよ');
+//   } else {
+//     console.log('5だよ');   
+//   }
+
+var count=0;
+$(document).ready(function(){ 
+    count++;
+    $('#test').html(count);
+
+})
